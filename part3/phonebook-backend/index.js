e94ebf576614f3c3866b6b6864f8614e9af1c61d
mtokan <tahsinokan@outlook.com>
2024-02-13 23:53:09 +0300

    add phonebook frontend build to backend

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,7 @@ morgan.token('body', req => JSON.stringify(req.body))
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('dist'))
 app.use(cors());
 
 let persons = [
