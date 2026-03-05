2d0ee43c636d5b4ea10acdb15aba68a71e0d0a43
mtokan <tahsinokan@outlook.com>
2026-03-05 15:19:34 +0100

    add exercise 9.14

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,7 +1,8 @@
-import { Router, Response } from 'express';
+import {Router, Response, Request, NextFunction} from 'express';
 import patientService from "../services/patientService";
-import { PatientWithoutSSN } from '../types';
-import {toNewPatient} from "../utils";
+import {NewPatient, PatientWithoutSSN} from '../types';
+import {NewPatientSchema} from "../utils";
+import z from "zod";
 
 
 const router = Router();
@@ -10,18 +11,27 @@ router.get('/', (_req, res: Response<PatientWithoutSSN[]>) => {
     res.send(patientService.getPatientsWithoutSSN());
 });
 
-router.post('/', (req, res: Response<PatientWithoutSSN|string>) => {
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
     try {
-        const newPatient = toNewPatient(req.body);
-        const patientWithoutSSN = patientService.addPatient(newPatient);
-        res.json(patientWithoutSSN);
-    } catch (error: unknown) {
-        let errorMessage = 'Error adding patient';
-        if (error instanceof Error) {
-            errorMessage += ' Error: ' + error.message;
-        }
-        res.status(400).send(errorMessage);
+        req.body = NewPatientSchema.parse(req.body);
+        next();
+    } catch (e: unknown) {
+        next(e);
     }
+};
+
+const errorMiddleware = (err: unknown, _req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof z.ZodError) {
+        res.status(400).send({ error: err.issues });
+    } else {
+        next(err);
+    }
+};
+
+router.post('/', newPatientParser, (req: Request<unknown,unknown,NewPatient>, res: Response<PatientWithoutSSN>) => {
+    res.json(patientService.addPatient(req.body));
 });
+
+router.use(errorMiddleware);
 
 export default router;
--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,51 +1,12 @@
 import {Gender, NewPatient} from "./types";
+import z from "zod";
 
-const isString = (value: unknown): value is string => typeof value === 'string' || value instanceof String;
+export const NewPatientSchema = z.object({
+    name: z.string(),
+    dateOfBirth: z.iso.date(),
+    gender: z.enum(Gender),
+    occupation: z.string(),
+    ssn: z.string(),
+});
 
-const isDate = (value: string): boolean => Boolean(Date.parse(value));
-
-const isGender = (value: string): value is Gender => Object.values(Gender).map(g => g.toString()).includes(value);
-
-const parseString = (object: unknown): string => {
-    if (!object || !isString(object)) {
-        throw new Error('Incorrect or missing data');
-    }
-
-    return object;
-};
-
-const parseDate = (date: unknown): string => {
-    if (!date || !isString(date) || !isDate(date)) {
-        throw new Error('Incorrect or missing date: ' + date);
-    }
-    return date;
-};
-
-const parseGender = (gender: unknown): Gender => {
-    if (!gender || !isString(gender) || !isGender(gender)) {
-        throw new Error('Incorrect or missing gender: ' + gender);
-    }
-    return gender;
-};
-
-export const toNewPatient = (object:unknown): NewPatient => {
-    if (!object || typeof object !== 'object') {
-        throw new Error('Incorrect or missing data');
-    }
-
-    if ('name' in object &&
-        'dateOfBirth' in object &&
-        'gender' in object &&
-        'occupation' in object &&
-        'ssn' in object) {
-        return {
-            name: parseString(object.name),
-            dateOfBirth: parseDate(object.dateOfBirth),
-            gender: parseGender(object.gender),
-            occupation: parseString(object.occupation),
-            ssn: parseString(object.ssn)
-        };
-    }
-
-    throw new Error('Incorrect data: some fields are missing');
-};
+export const toNewPatient = (obj: unknown): NewPatient => NewPatientSchema.parse(obj);
